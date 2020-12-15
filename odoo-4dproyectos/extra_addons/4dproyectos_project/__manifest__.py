34d87c9d301af5e4daf3426bbc4bacd3a026e9e0
Juan López Corominas <juan.lopez@eticco.es>
2020-12-15 11:53:55 +0100

    Personalización básica de proyectos

---
+++ odoo-4dproyectos/extra_addons/4dproyectos_project/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "4D Proyectos Proyecto",
+    'summary': """Personalización de proyectos de 4D Proyectos""",
+    'author': "Eticco Freelosophy",
+    'license': 'AGPL-3',
+    'website': "",
+    'category': 'Eticco',
+    'version': '14.0.1',
+    'depends': [
+        'project',
+    ],
+    'data': [
+            'views/project_view.xml',
+        
+    ],
+    'demo': [],
+    'application': False,
+    'installable': True
+}
---
+++ odoo-4dproyectos/extra_addons/4dproyectos_project/models/project.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+import logging
+_logger = logging.getLogger(__name__)
+
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    partner_shipping_id = fields.Many2one('res.partner', string='Dirección')
+    total_budget = fields.Float(string="Presupuesto total")
+    raw_material_cost = fields.Float(string="Coste materias primas")
+    status = fields.Selection(string="Estado", selection=[('contratada', 'Contratada'), ('en_ejecucion', 'En ejecución'), ('terminada_en_fabrica', 'Terminada en fábrica'), ('entregada', 'Entregada'), ('facturada', 'Facturada'), ('cerrada', 'Cerrada')])
+    start_date = fields.Date(string="Fecha de alta")

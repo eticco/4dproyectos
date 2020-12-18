3328fd53a3b073b4ff57bef579db5e351822f869
Juan López Corominas <juan.lopez@eticco.es>
2020-12-18 18:19:06 +0100

    Que se copie en la creación y la actualización

--- odoo-4dproyectos/extra_addons/4dproyectos_hr/models/hr_employee.py
+++ odoo-4dproyectos/extra_addons/4dproyectos_hr/models/hr_employee.py
@@ -6,7 +6,7 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
     
     @api.model
-    def create(self, values):
+    def write(self, values):
         if values.get('job_id', False):
             values['timesheet_cost'] = self.env['hr.job'].browse(values.get('job_id')).timesheet_cost
         result = super(HrEmployee, self).create(values)

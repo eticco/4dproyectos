04ea436ec1327e0438d61b678ce626ee75563f18
Juan López Corominas <juan.lopez@eticco.es>
2020-12-18 13:15:39 +0100

    Primera versión de módulo para generar QRs

---
+++ odoo-4dproyectos/extra_addons/4dproyectos_qr/__manifest__.py
@@ -0,0 +1,13 @@
+ # -*- coding: utf-8 -*-
+{
+    'name': 'Qweb QR',
+    'version': '1.0',
+    'category': 'Reporting',
+    'license': 'AGPL-3',
+    'summary': 'Nuevos Qweb de códigos QR',
+    'author': 'Eticco Freelosophy',
+    'website': 'https://www.eticco.es',
+    'depends': ['mrp'],
+    'data': ['report/reports.xml', 'data/qr_reports.xml'],
+    'installable': True,
+}

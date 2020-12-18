14796d90bb57814cd9e4f539f935aa4787c67179
Juan López Corominas <juan.lopez@eticco.es>
2020-12-18 19:57:34 +0100

    Funcionando en Odoo 14, sin decoradores

--- odoo-4dproyectos/extra_addons/4dproyectos_hr/models/hr_employee.py
+++ odoo-4dproyectos/extra_addons/4dproyectos_hr/models/hr_employee.py
@@ -5,9 +5,9 @@ from odoo import models, api
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
     
-    @api.model
     def write(self, values):
-        if values.get('job_id', False):
-            values['timesheet_cost'] = self.env['hr.job'].browse(values.get('job_id')).timesheet_cost
         result = super(HrEmployee, self).write(values)
+        if values.get('job_id', False):
+            for employee in self:
+                employee.timesheet_cost = employee.env['hr.job'].browse(values.get('job_id')).timesheet_cost
         return result

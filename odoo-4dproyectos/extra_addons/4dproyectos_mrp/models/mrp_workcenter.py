80c696e359f8d38a4af3fe390d4a443f6a528918
Juan López Corominas <juan.lopez@eticco.es>
2020-12-29 13:22:25 +0100

    Campos necesarios para #10274

--- odoo-4dproyectos/extra_addons/4dproyectos_mrp/models/mrp_workcenter.py
+++ odoo-4dproyectos/extra_addons/4dproyectos_mrp/models/mrp_workcenter.py
@@ -17,5 +17,5 @@ class MrpWorkcenterProductivity(models.Model):
     def _compute_user_cost(self):
         for line in self:
             employee_id = self.env['hr.employee'].search([('user_id', '=', line.user_id.id)], limit=1)
-            line.user_cost = line.duration * employee_id.timesheet_cost
+            line.user_cost = line.duration * employee_id.timesheet_cost / 60.0
     
--- odoo-4dproyectos/extra_addons/4dproyectos_mrp/models/mrp_workorder.py
+++ odoo-4dproyectos/extra_addons/4dproyectos_mrp/models/mrp_workorder.py
@@ -7,14 +7,33 @@ class MrpWorkorder(models.Model):
     
     workcenter_cost = fields.Float(string='Coste/máquina', compute='_compute_workcenter_cost')
     users_cost = fields.Float(string='Coste/empleados', compute='_compute_users_cost')
+    
+    duration_per_unit = fields.Float(string='Duración/unidad', compute='_compute_per_unit')
+    workcenter_cost_per_unit =fields.Float(string='Coste/máquina unitario', compute='_compute_per_unit')
+    users_cost_per_unit = fields.Float(string='Coste/empleados unitario', compute='_compute_per_unit')
 
     @api.depends('duration', 'workcenter_id')
     def _compute_workcenter_cost(self):
         for order in self:
-            order.workcenter_cost = order.duration * order.workcenter_id.costs_hour
+            order.workcenter_cost = order.duration * order.workcenter_id.costs_hour / 60.0
 
     @api.depends('time_ids.user_cost')
     def _compute_users_cost(self):
         for order in self:
             order.users_cost = sum(order.time_ids.mapped('user_cost'))
+
+    @api.depends('duration')
+    def _compute_per_unit(self):
+        for order in self:
+            for criteria_id in order.production_id.criteria_amount_ids:
+                if criteria_id.computation_criteria_id.id == order.workcenter_id.computation_criteria_id.id:
+                    if criteria_id.amount > 0:
+                        order.duration_per_unit = order.duration / criteria_id.amount
+                        order.workcenter_cost_per_unit = order.workcenter_cost / criteria_id.amount
+                        order.users_cost_per_unit = order.users_cost / criteria_id.amount
+                    else:
+                        order.duration_per_unit = 0.0
+                        order.workcenter_cost_per_unit = 0.0
+                        order.users_cost_per_unit = 0.0
+                    break
 

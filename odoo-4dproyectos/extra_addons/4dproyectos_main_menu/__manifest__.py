2a31238840f0e52b3776abbeedfd97c8c576d741
Juan López Corominas <juan.lopez@eticco.es>
2021-04-19 17:27:43 +0200

    Soluciona #12245

---
+++ odoo-4dproyectos/extra_addons/4dproyectos_main_menu/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "4D Proyectos Modificar menú",
+    'summary': """Personalización del menú principal para 4D Proyectos""",
+    'author': "Eticco Freelosophy",
+    'license': 'AGPL-3',
+    'website': "",
+    'category': 'Eticco',
+    'version': '14.0.1',
+    'depends': [
+        'base',
+    ],
+    'data': [
+            'views/main_menu_view.xml',
+    ],
+    'demo': [],
+    'application': False,
+    'installable': True
+}

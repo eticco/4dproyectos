19bea0374121ac93e464ff27eb39d222828de16a
David M <david.mendez@eticco.com>
2021-05-20 09:14:34 +0200

    Crear Módulo 4d_proyectos_reports

---
+++ odoo-4dproyectos/extra_addons/4dproyectos_reports/__manifest__.py
@@ -0,0 +1,15 @@
+ # -*- coding: utf-8 -*-
+{
+    'name': 'Informes 4D',
+    'version': '1.0',
+    'category': 'Reporting',
+    'license': 'AGPL-3',
+    'summary': 'Informes 4D Proyectos',
+    'author': 'Eticco Freelosophy',
+    'website': 'https://www.eticco.es',
+    'depends': [
+        'mrp',
+    ],
+    'data': ['reports/production_order_report.xml', 'reports/reports.xml'],
+    'installable': True,
+}

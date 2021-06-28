36f680e7e72a8560a0a165ead11e5e0ba4871015
Juan López Corominas <juan.lopez@eticco.es>
2021-06-28 11:54:47 +0200

    Traducciones pendientes indicadas por Lorena

--- odoo-4dproyectos/extra_addons/4dproyectos_hr/models/hr_job.py
+++ odoo-4dproyectos/extra_addons/4dproyectos_hr/models/hr_job.py
@@ -5,6 +5,6 @@ from odoo import models, fields
 class HrJob(models.Model):
     _inherit = 'hr.job'
     
-    timesheet_cost = fields.Monetary('Timesheet Cost', currency_field='currency_id',
+    timesheet_cost = fields.Monetary('Coste del parte de horas', currency_field='currency_id',
         groups="hr.group_hr_user", default=0.0)
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id', readonly=True)
